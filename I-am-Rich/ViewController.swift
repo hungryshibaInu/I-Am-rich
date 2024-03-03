7940ba9997ae05d7f7701453ba91670692ba315b
hungryshibamacair <prom.p2001@gmail.com>
2024-03-03 17:48:07 +0700

    Initial Commit

---
+++ I-am-Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-Rich
+//
+//  Created by 3promm on 23/2/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
